ed3a77bc069e146a0625c39eb296bcd7eb740ea0
Jatin Foujdar <jatin321001@gmail.com>
2024-11-01 14:46:22 +0530

    Initial Commit

---
+++ Era Odyssey/Era_OdysseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Era_OdysseyApp.swift
+//  Era Odyssey
+//
+//  Created by jatin foujdar on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Era_OdysseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
