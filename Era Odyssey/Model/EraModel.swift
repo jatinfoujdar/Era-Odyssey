865ee4e86a32dbc87fbf17e84eb3df5daa7fd9f1
Jatin Foujdar <jatin321001@gmail.com>
2024-11-03 18:58:16 +0530

    data model commit

--- Era Odyssey/Model/EraModel.swift
+++ Era Odyssey/Model/EraModel.swift
@@ -5,3 +5,17 @@
 //  Created by jatin foujdar on 01/11/24.
 //
 
+import Foundation
+
+struct EraModel: Identifiable, Codable {
+    var id: Int
+    let name: String
+    let image: String
+    let description: String
+    let keyDates: [String]
+}
+
+
+
+
+
